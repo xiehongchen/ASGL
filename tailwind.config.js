6ffd62ea1effbd941144be5ba133488bac506272
xiehongchen <1754581057@qq.com>
2024-04-17 18:09:03 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      padding: {
+        '1' : '30px'
+      }
+    },
+  },
+  plugins: [],
+}
+
