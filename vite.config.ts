e33d47003a27d52ba03af333fb6f27aa875d5b27
xiehongchen <1754581057@qq.com>
2024-04-24 18:15:29 +0800

    vite 修改base

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const outputPath = process.env.NODE_ENV === 'production' ? '/ASGL' : ''
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: outputPath,
   resolve: {
     alias: {
       '@': '/src',
