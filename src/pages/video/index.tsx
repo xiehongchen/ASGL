f3327b5d513f43fc280dda8ed73f89c559150f64
xiehongchen <1754581057@qq.com>
2024-05-16 19:02:54 +0800

    视频

--- src/pages/video/index.tsx
+++ src/pages/video/index.tsx
@@ -1,65 +1,64 @@
-import JoLPlayer, { callBackType, JoLPlayerRef, qualityKey } from "jol-player";
-import { useRef, useEffect } from "react";
+import React, { useState, useRef } from 'react';
+import { Select } from 'antd';
 import './index.scss'
-export default function Video() {
-  const videoRef = useRef<JoLPlayerRef>(null!);
-  const onProgressMouseUp: callBackType = (val) => {
-    console.log(`onProgressMouseUp`, val);
+function VideoPlayer() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const changeSpeed = (newSpeed: number) => {
+    console.log('newSpeed', newSpeed)
+    if (videoRef.current) {
+      videoRef.current.playbackRate = newSpeed;
+    }
   };
-  const onEndEd: callBackType = (val) => {
-    console.log(`onEndEd`, val);
+  const [file, setFile] = useState<File | null>(null);
+  const onchange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('event', event)
+    const selectedFile = event.target.files && event.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
   };
-  const onPause: callBackType = (val) => {
-    console.log(`onPause`, val);
-  };
-  const onProgressMouseDown: callBackType = (val) => {
-    console.log(`onProgressMouseDown`, val);
-  };
-  const onPlay: callBackType = (val) => {
-    console.log(`onPlay`, val);
-  };
-  const onTimeChange: callBackType = (val) => {
-    console.log(`onTimeChange`, val);
-  };
-  const onvolumechange: callBackType = (val) => {
-    console.log(`onvolumechange`, val);
-  };
-  const onError = () => {
-    console.log(`onError`);
-  };
-  const onQualityChange: callBackType<qualityKey> = (val) => {
-    console.log(`onQualityChange`, val);
-  };
-  useEffect(() => {
-    console.log(`videoRef.current`, videoRef.current);
-  }, [videoRef.current]);
+
   return (
-    <div className="content">
-      <JoLPlayer
-        ref={videoRef}
-        onProgressMouseUp={onProgressMouseUp}
-        onEndEd={onEndEd}
-        onPause={onPause}
-        onProgressMouseDown={onProgressMouseDown}
-        onPlay={onPlay}
-        onTimeChange={onTimeChange}
-        onvolumechange={onvolumechange}
-        onError={onError}
-        onQualityChange={onQualityChange}
-        option={{
-          videoSrc:
-            "https://gs-files.oss-cn-hongkong.aliyuncs.com/okr/prod/file/2021/08/31/540p.mp4",
-          width: 300,
-          // height: 100,
-          mode: 'widthFix',
-          theme: "#C91314",
-          pausePlacement: "center",
-          isToast: true,
-          isShowScreenshot: false,
-          isShowPicture: false,
-          isProgressFloat: true
-        }}
-      />
+    <div className='content'>
+      <video
+        className='video'
+        ref={videoRef} src='https://gs-files.oss-cn-hongkong.aliyuncs.com/okr/prod/file/2021/08/31/540p.mp4'
+        width="320" height="240"
+        controls>
+      </video>
+      <div>
+        <span>倍数：</span>
+        <Select
+          defaultValue={1}
+          style={{ width: 120 }}
+          onChange={changeSpeed}
+          options={[
+            { value: '0.5', label: '0.5' },
+            { value: '1', label: '1' },
+            { value: '1.5', label: '1.5' },
+            { value: '2', label: '2' },
+            { value: '3', label: '3' },
+            { value: '5', label: '5' },
+          ]}
+        />
+      </div>
+      <div>
+        <input type="file" onChange={onchange}/>
+        {file && (
+        <div>
+          <p>文件信息:</p>
+          <ul>
+            {Object.keys(file).map((key) => (
+              <li key={key}>
+                <strong>{key}:</strong> {file[key as keyof File]}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      </div>
     </div>
   );
 }
+
+export default VideoPlayer;
